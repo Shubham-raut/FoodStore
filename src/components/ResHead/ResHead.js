e0f46a70eab29760de1b77670a99bf0e5ad99b07
Shubham-raut <123shubhamraut@gmail.com>
2020-10-11 12:05:07 +0530

    Restaurent head

--- src/components/ResHead/ResHead.js
+++ src/components/ResHead/ResHead.js
@@ -3,14 +3,14 @@ import { useSelector } from 'react-redux';
 import "./ResHead.css";
 
 const Reshead = () => {
-    const clickedRes = useSelector(state => state.clickedRes);
+    const currentRes = useSelector(state => state.restaurentState.currentRes);
 
     let imageUrl = "https://b.zmtcdn.com/data/res_imagery/307113_RESTAURANT_e2f599e624e5e8c01404af7608728671.jpg?output-format=webp";
 
 
-    let imgUrl = clickedRes ?
-        clickedRes.featured_image ?
-            clickedRes.featured_image :
+    let imgUrl = currentRes ?
+        currentRes.featured_image ?
+            currentRes.featured_image :
             imageUrl :
         imageUrl;
 
@@ -21,14 +21,14 @@ const Reshead = () => {
                     <img src={imgUrl} alt="Restraurent Img" />
                 </div>
                 <div className="resDetails_right_container">
-                    <div className="res_titleX">{clickedRes.name}</div>
-                    <div className='res_cuisinesX'>{clickedRes.cuisines}</div>
-                    <div className='res_locationX'>{clickedRes.location.locality}</div>
-                    <div className='res_timingX'>{clickedRes.timings}</div>
+                    <div className="res_titleX">{currentRes.name}</div>
+                    <div className='res_cuisinesX'>{currentRes.cuisines}</div>
+                    <div className='res_locationX'>{currentRes.location.locality}</div>
+                    <div className='res_timingX'>{currentRes.timings}</div>
 
                     <div className="cost_rateX">
-                        <div className='res_ratingX'><i className="fa fa-star" aria-hidden="true"></i> {clickedRes.user_rating.aggregate_rating}<span className="res_ratingX_sub">{clickedRes.user_rating.votes}+ Ratings</span></div>
-                        <div className='res_costX'><i className="fa fa-inr" aria-hidden="true"></i> {clickedRes.average_cost_for_two}<span className="res_costX_sub">Cost for Two</span></div>
+                        <div className='res_ratingX'><i className="fa fa-star" aria-hidden="true"></i> {currentRes.user_rating.aggregate_rating}<span className="res_ratingX_sub">{currentRes.user_rating.votes}+ Ratings</span></div>
+                        <div className='res_costX'><i className="fa fa-inr" aria-hidden="true"></i> {currentRes.average_cost_for_two}<span className="res_costX_sub">Cost for Two</span></div>
                     </div>
                 </div>
             </div>
@@ -37,3 +37,26 @@ const Reshead = () => {
 }
 
 export default Reshead;
+
+
+export const ResheadLoad = () => {
+
+    let load = [];
+    load.push(
+        <div className="resDetails_container">
+            <div className="resDetails_inner_container">
+                <div className="resimgarea_load width"></div>
+                <div className="resDetails_right_container_load">
+                    <div className="restitle_load margin"></div>
+                    <div className="restop_load"></div>
+                    <div className='resmid_load'></div>
+                    <div className='resBot_load'></div>
+                </div>
+            </div>
+        </div>
+    );
+    return (
+        <> { load} </>
+    );
+
+}
