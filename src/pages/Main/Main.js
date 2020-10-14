37b8ffdff8b277c998e9a9d0cb1bfe34de07e646
Shubham-raut <123shubhamraut@gmail.com>
2020-10-14 10:17:29 +0530

    main page

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
-import CitySearch from '../../components/CitySearch/CitySearch';
-import DishSearch from '../../components/DishSearch/DishSearch';
-import Footer from '../../components/Footer/Footer';
-import './Main.css';
-// import Header from '../../components/Header/Header';
+import React from 'react';
+import "./Main.css";
+import { useSelector, useDispatch } from 'react-redux';
+import Filters from '../../components/Filters/Filters';
+import RestCard, { RestCardLoad } from '../../components/RestCard/RestCard';
 
 const Main = () => {
+
+    // const dispatch = useDispatch();
+    const resData = useSelector(state => state.cityState.resData);
+    const loading = useSelector(state => state.cityState.loading);
+    const error = useSelector(state => state.cityState.error);
 
     return (
         <div id="main">
 
-            <section id="mainCover">
-                <div className="coverImg"></div>
-                <div className="coverContent">
-                    <h1 className="coverTitle">You order we deliver</h1>
-                    <p className="coverDesc">Order food from favourite restaurants near you</p>
-                    <CitySearch />
-                </div>
-            </section>
+            <Filters />
 
-            <section id="mid">
-                Mid Section
-            </section>
+            <div className="mainPage_card_container">
+                {loading ?
+                    < RestCardLoad /> :
+                    (!error) ?
+                        (resData != null && resData.length !== 0) ?
+                            resData.map((items) =>
+                                <RestCard items={items} key={items.restaurant.R.res_id} />
+                            ) :
+                            <div className='notFound'>
+                                <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                                <span>No results found</span>
+                            </div> :
+                        <div className="error">
+                            <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                            <span>{error}</span>
+                        </div>
+                }
 
-            <Footer />
+            </div>
+
         </div>
     );
 }
