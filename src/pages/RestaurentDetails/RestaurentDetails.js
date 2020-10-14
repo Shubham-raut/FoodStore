178380c8f2b41e9ac29b772d2f27cdee461ff992
Shubham-raut <123shubhamraut@gmail.com>
2020-10-14 10:17:49 +0530

    Res details page

--- src/pages/RestaurentDetails/RestaurentDetails.js
+++ src/pages/RestaurentDetails/RestaurentDetails.js
@@ -1,28 +1,57 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import "./RestaurentDetails.css";
-import Reshead from '../../components/ResHead/ResHead';
-import ResMenu from '../../components/ResMenu/ResMenu';
+import Reshead, { ResheadLoad } from '../../components/ResHead/ResHead';
+import ResMenu, { ResMenuLoad } from '../../components/ResMenu/ResMenu';
+import Cart from '../../components/Cart/Cart';
 
 const RestaurentDetails = () => {
-    const clickedRes = useSelector(state => state.clickedRes);
-    const resMenu = useSelector(state => state.resMenu);
+    const currentRes = useSelector(state => state.restaurentState.currentRes);
+    const resMenu = useSelector(state => state.restaurentState.resMenu);
+    const resHeadLoading = useSelector(state => state.restaurentState.resHeadLoading);
+    const resMenuLoading = useSelector(state => state.restaurentState.resMenuLoading);
+    const resHeadError = useSelector(state => state.restaurentState.resHeadError);
+    const resMenuError = useSelector(state => state.restaurentState.resMenuError);
 
     return (
-        <div id="clickedRestaurent">
-            {clickedRes ? (
-                <Reshead />
-            ) : (
-                    <p>loading...</p>
-                )}
+        <div id="currentRestaurent">
+            {resHeadLoading ?
+                <ResheadLoad /> :
+                (!resHeadError) ?
+                    currentRes ?
+                        <Reshead /> :
+                        <div className='notFound'>
+                            <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                            <span>No results found</span>
+                        </div> :
+                    <div className="error">
+                        <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                        <span>{resHeadError}</span>
+                    </div>
 
+            }
+            <div className='resElements'>
+                <div className='dishContainer'>
+                    {resMenuLoading ?
+                        <ResMenuLoad /> :
+                        (!resMenuError) ?
 
-            {resMenu.length ? (
-                resMenu.map((item, index) =>
-                    <ResMenu item={item} index={index} />
-                )) : (
-                    <p>loading...</p>
-                )}
+                            resMenu.length ?
+                                resMenu.map((item, index) =>
+                                    <ResMenu item={item} index={index} />
+                                ) :
+                                <div className='notFound'>
+                                    <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                                    <span>No results found</span>
+                                </div> :
+                            <div className="error">
+                                <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                                <span>{resMenuError}</span>
+                            </div>
+                    }
+                </div>
+                <Cart />
+            </div>
 
         </div>
     );
